b373be23ea02465eb0d187030beeb5b2bb57f475
Karen Cheng <karencheng@dhcp-v880-17a-00457.1Xwireless.unc.edu>
2017-03-28 17:38:44 -0400

    fixed create users table

--- database/migrations/2017_03_28_211934_add-players.php
+++ database/migrations/2017_03_28_211934_add-players.php
@@ -13,7 +13,22 @@ class AddPlayers extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('players', function (Blueprint $table) {
+          $table->increments('id');
+          $table->string('first')->default('');
+          $table->string('last')->default('');
+          $table->integer('gamesPlayed')->default(0);
+          $table->integer('avgMinutes')->default(0);
+          $table->integer('ppg')->default(0);
+          $table->integer('apg')->default(0);
+          $table->integer('spg')->default(0);
+          $table->integer('bpg')->default(0);
+          $table->integer('tpg')->default(0);
+          $table->float('fgPercent')->default(0.00);
+          $table->float('ftPercent')->default(0.00);
+          $table->float('tpPercent')->default(0.00);
+          $table->timestamps();
+        });
     }
 
     /**
@@ -23,6 +38,6 @@ class AddPlayers extends Migration
      */
     public function down()
     {
-        //
+        Schema::dropIfExists('players');
     }
 }
